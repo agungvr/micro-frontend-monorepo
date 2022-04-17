1e709cc110600f581a4f17b5107d3412202a27d3
Agung Nugraha <agungvr@gmail.com>
2022-04-17 23:58:13 +0700

    [ADD] server.js

---
+++ packages/micro-frontend-app1/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+
+const server = express();
+
+server.use(express.static(path.join(__dirname, "dist")));
+
+server.listen(PORT, () => console.log(`Listening on ${PORT}`));
